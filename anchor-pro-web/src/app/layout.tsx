bc6d00d2b720017e4a38436e98c199d4254b883d
simwi <simwinga8788@gmail.com>
2026-04-23 12:05:47 +0200

    fix: Restore missing body tag in layout.tsx

--- anchor-pro-web/src/app/layout.tsx
+++ anchor-pro-web/src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
+      <body>
         <AuthProvider>
           <DictionaryProvider>
             {children}
